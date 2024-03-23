f24deea925a5b7dfa1d8e4efd4e56d2d3528d28c
mussie haile <44857352+mussiehaile@users.noreply.github.com>
2024-03-23 17:16:36 +0300

    Update config_static_files.py

--- config/config_static_files.py
+++ config/config_static_files.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 @dataclass
 class ConfigStaticFiles:
     DB_DATABASE:str ="my_db"
-    DB_USER:str ="mussie"
-    DB_PASSWORD:str="moss"
+    DB_USER:str ="user"
+    DB_PASSWORD:str="xxx"
     vegitable_discription: str = f"Vegetables are parts of plants that are consumed by humans or other animals as food."
     fruit_discription:str = f"Fruits are The sweet and fleshy product of a tree or other plant that contains seed and can be eaten as food."
